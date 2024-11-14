342e96f35527df0860c4d4ce9fa18af1751f5dd7
Ray Carter <raycarter158@gmail.com>
2024-11-14 15:48:45 +0000

    modify settings.py file

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0bj2--ye#3r^!aja=-vene0@$r55m+@l$*mc%564dllc2ob1+$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-raycarter23-djangoproje-mralcgchnnq.ws.codeinstitute-ide.net','.herokuapp.com']
 
